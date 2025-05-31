68bf019a4689573e127f576c3ef4a9f2d712c4b2
ganggyunggyu <qwzx16@naver.com>
2025-05-31 20:23:33 +0900

    chore: certmodule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { CertModule } from './cert/cert.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/your-db-name'),
+    MongooseModule.forRoot(
+      'mongodb+srv://kkk819:12qwaszx@cluster0.uw5n95x.mongodb.net/qit',
+    ),
+
     AuthModule,
     UserModule,
     ConfigModule,
+    CertModule,
   ],
   controllers: [AppController],
   providers: [AppService],
